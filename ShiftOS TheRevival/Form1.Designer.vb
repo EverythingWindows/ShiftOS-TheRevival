e8ed87624464e628ae0a713f1d957720a8ab1b03
alifm <alifm@LAPTOP-DENPKDP>
2024-08-19 16:56:25 +0700

    Add project files.

---
+++ ShiftOS TheRevival/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(25, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(157, 16)
+        Label1.TabIndex = 0
+        Label1.Text = "ShiftOS TheRevival is revivde"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(25, 49)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(107, 16)
+        Label2.TabIndex = 1
+        Label2.Text = "nothing in here yet"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(25, 84)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(135, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "button for buttoning"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 16F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(344, 140)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
